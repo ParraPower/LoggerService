cfb61393ccc610d7c41d6924fcd9ea1c83d91ef1
Ahmed Alameddine <ahmed.alameddine2016@outlook.com>
2023-08-24 16:56:29 +1000

    Add project files.

---
+++ LoggerService/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using LoggerService.Implementations;
+using LoggerService.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoggerService.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
